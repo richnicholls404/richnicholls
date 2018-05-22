afba9f8a5466654589e4ae359ac9e811dc2586aa
Rich Nicholls <richnicholls404@gmail.com>
2018-05-23 00:00:21 +0100

    home git banner

--- ui/components/home/Home.js
+++ ui/components/home/Home.js
@@ -9,6 +9,20 @@ export default class extends Component {
 		return (
 			<section className={`${this.className()} h-100 text-white`}>
 				<a name="home" href="#home" />
+
+				<a href="https://github.com/richnicholls404/richnicholls">
+					<img
+						style={{
+							position: 'absolute',
+							top: 0,
+							right: 0,
+							border: 0,
+						}}
+						src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+						alt="Fork me on GitHub"
+					/>
+				</a>
+
 				<div className="h-100 container d-flex flex-column justify-content-center">
 					<h1 className="mb-3">Hello, I&rsquo;m <span className="text-theme">Rich Nicholls</span></h1>
 					<p>I&rsquo;m a full stack web developer from the wilds of Wiltshire, England.</p>
