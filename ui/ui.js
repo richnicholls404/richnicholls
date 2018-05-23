54a637d3c83aeb4245815200fd9a88b3ba6bee95
Rich Nicholls <richnicholls404@gmail.com>
2018-05-23 08:03:14 +0100

    refactor to move app to a component dir

--- ui/ui.js
+++ ui/ui.js
@@ -7,7 +7,7 @@ import 'bootstrap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+import App from './components/app/App';
 
 window.$ = $;
 window.jQuery = jQuery;
